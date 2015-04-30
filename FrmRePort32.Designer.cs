62b5c5e5a9714030256c816a14f422cfdd5867b5
Kuroko Hikikomori <kuronekohiki@gmail.com>
2015-04-30 10:36:08 +0700

    แก้ไข ใบเสร็จ

--- FrmRePort32.Designer.cs
+++ FrmRePort32.Designer.cs
@@ -28,88 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.crystalReportViewer2 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.crystalReportViewer3 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.dTP_Date = new System.Windows.Forms.DateTimePicker();
             this.dTP_ToDate = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.bt_Search = new System.Windows.Forms.Button();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.crystalReportViewer3 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.crystalReportViewer2 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.tabPage3.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // dTP_Date
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Location = new System.Drawing.Point(12, 63);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(984, 597);
-            this.tabControl1.TabIndex = 0;
+            this.dTP_Date.Location = new System.Drawing.Point(264, 15);
+            this.dTP_Date.Name = "dTP_Date";
+            this.dTP_Date.Size = new System.Drawing.Size(200, 26);
+            this.dTP_Date.TabIndex = 1;
             // 
-            // tabPage1
+            // dTP_ToDate
             // 
-            this.tabPage1.Controls.Add(this.crystalReportViewer1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 27);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(976, 566);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "รายจ่าย";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.dTP_ToDate.Location = new System.Drawing.Point(554, 15);
+            this.dTP_ToDate.Name = "dTP_ToDate";
+            this.dTP_ToDate.Size = new System.Drawing.Size(200, 26);
+            this.dTP_ToDate.TabIndex = 2;
             // 
-            // crystalReportViewer1
+            // label1
             // 
-            this.crystalReportViewer1.ActiveViewIndex = -1;
-            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.crystalReportViewer1.Location = new System.Drawing.Point(3, 3);
-            this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.ShowCloseButton = false;
-            this.crystalReportViewer1.ShowGroupTreeButton = false;
-            this.crystalReportViewer1.ShowLogo = false;
-            this.crystalReportViewer1.ShowParameterPanelButton = false;
-            this.crystalReportViewer1.Size = new System.Drawing.Size(970, 560);
-            this.crystalReportViewer1.TabIndex = 0;
-            this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ตั้งแต่วันที่";
             // 
-            // tabPage2
+            // label2
             // 
-            this.tabPage2.Controls.Add(this.crystalReportViewer2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 27);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(976, 566);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "รายรับ(บริการ)";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(497, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ถึงวันที่";
             // 
-            // crystalReportViewer2
+            // bt_Search
             // 
-            this.crystalReportViewer2.ActiveViewIndex = -1;
-            this.crystalReportViewer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.crystalReportViewer2.Cursor = System.Windows.Forms.Cursors.Default;
-            this.crystalReportViewer2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.crystalReportViewer2.Location = new System.Drawing.Point(3, 3);
-            this.crystalReportViewer2.Name = "crystalReportViewer2";
-            this.crystalReportViewer2.ShowCloseButton = false;
-            this.crystalReportViewer2.ShowGroupTreeButton = false;
-            this.crystalReportViewer2.ShowLogo = false;
-            this.crystalReportViewer2.ShowParameterPanelButton = false;
-            this.crystalReportViewer2.Size = new System.Drawing.Size(970, 560);
-            this.crystalReportViewer2.TabIndex = 0;
-            this.crystalReportViewer2.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            this.bt_Search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_Search.Location = new System.Drawing.Point(760, 12);
+            this.bt_Search.Name = "bt_Search";
+            this.bt_Search.Size = new System.Drawing.Size(75, 32);
+            this.bt_Search.TabIndex = 5;
+            this.bt_Search.Text = "ค้นหา";
+            this.bt_Search.UseVisualStyleBackColor = true;
+            this.bt_Search.Click += new System.EventHandler(this.bt_Search_Click);
             // 
             // tabPage3
             // 
@@ -138,48 +120,102 @@
             this.crystalReportViewer3.TabIndex = 0;
             this.crystalReportViewer3.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
             // 
-            // dTP_Date
+            // tabPage2
             // 
-            this.dTP_Date.Location = new System.Drawing.Point(264, 15);
-            this.dTP_Date.Name = "dTP_Date";
-            this.dTP_Date.Size = new System.Drawing.Size(200, 26);
-            this.dTP_Date.TabIndex = 1;
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Controls.Add(this.radioButton2);
+            this.tabPage2.Controls.Add(this.radioButton1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(976, 566);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "รายรับ(บริการ)";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // dTP_ToDate
+            // radioButton1
             // 
-            this.dTP_ToDate.Location = new System.Drawing.Point(527, 15);
-            this.dTP_ToDate.Name = "dTP_ToDate";
-            this.dTP_ToDate.Size = new System.Drawing.Size(200, 26);
-            this.dTP_ToDate.TabIndex = 2;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 7);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(63, 22);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "บริการ";
+            this.radioButton1.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // radioButton2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(189, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 18);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "ตั้งแต่วันที่";
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(75, 7);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 22);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "ยารักษา";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // tabPage1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(470, 19);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 18);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "ถึงวันที่";
+            this.tabPage1.Controls.Add(this.crystalReportViewer1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 27);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(976, 566);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "รายจ่าย";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // bt_Search
+            // crystalReportViewer1
             // 
-            this.bt_Search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.bt_Search.Location = new System.Drawing.Point(733, 12);
-            this.bt_Search.Name = "bt_Search";
-            this.bt_Search.Size = new System.Drawing.Size(75, 32);
-            this.bt_Search.TabIndex = 5;
-            this.bt_Search.Text = "ค้นหา";
-            this.bt_Search.UseVisualStyleBackColor = true;
-            this.bt_Search.Click += new System.EventHandler(this.bt_Search_Click);
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(3, 3);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ShowCloseButton = false;
+            this.crystalReportViewer1.ShowGroupTreeButton = false;
+            this.crystalReportViewer1.ShowLogo = false;
+            this.crystalReportViewer1.ShowParameterPanelButton = false;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(970, 560);
+            this.crystalReportViewer1.TabIndex = 0;
+            this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 63);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(984, 597);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.crystalReportViewer2);
+            this.panel1.Location = new System.Drawing.Point(6, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(964, 525);
+            this.panel1.TabIndex = 3;
+            // 
+            // crystalReportViewer2
+            // 
+            this.crystalReportViewer2.ActiveViewIndex = -1;
+            this.crystalReportViewer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer2.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer2.Name = "crystalReportViewer2";
+            this.crystalReportViewer2.ShowCloseButton = false;
+            this.crystalReportViewer2.ShowGroupTreeButton = false;
+            this.crystalReportViewer2.ShowLogo = false;
+            this.crystalReportViewer2.ShowParameterPanelButton = false;
+            this.crystalReportViewer2.Size = new System.Drawing.Size(964, 525);
+            this.crystalReportViewer2.TabIndex = 5;
+            this.crystalReportViewer2.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
             // 
             // FrmRePort32
             // 
@@ -198,10 +234,12 @@
             this.Text = "ดูรายรับ-รายจ่าย";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.FrmRePort32_Load);
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -209,17 +247,20 @@
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.DateTimePicker dTP_Date;
         private System.Windows.Forms.DateTimePicker dTP_ToDate;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button bt_Search;
         private System.Windows.Forms.TabPage tabPage3;
-        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
-        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer2;
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panel1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
--- FrmRePort32.cs
+++ FrmRePort32.cs
@@ -32,25 +32,29 @@ namespace Petshop
 
         private void SearchReport()
         {
+            //////////////////////ข้อมูลองค์กร//////////////////////////
+            DataTable idtCompany;
+            string isqlCompany = "SELECT * FROM petshop.tb_company";
+            idtCompany = iConnect.SelectByCommand(isqlCompany);
+            ////////////////////ช่วงเวลาที่โหลดข้อมูล//////////////////////////
             System.Globalization.CultureInfo cultureInfo = new System.Globalization.CultureInfo("en-US");
             System.Threading.Thread.CurrentThread.CurrentCulture = cultureInfo;
             System.Threading.Thread.CurrentThread.CurrentUICulture = cultureInfo;
             string idtpDate = dTP_Date.Value.ToString("yyyy-MM-dd");
             string idtpToDate = dTP_ToDate.Value.ToString("yyyy-MM-dd");
-            ///////////////////////////////////////////////////
+            ///////////////////////////[[รายงานส่วนแรก]]//////////////////////////////////
             DataTable idtOutlay;
-            string isqlOutlay = "SELECT tb_outlaydetail.*,tb_outlay.outlay_Detail,tb_employee.Em_Name FROM tb_outlaydetail,tb_outlay,tb_employee where tb_outlay.outlay_ID = tb_outlaydetail.outlay_ID AND tb_outlaydetail.Em_ID = tb_employee.Em_ID AND (STR_TO_DATE(`outlayDetail_Date`,'%Y-%m-%d') between  '"+idtpDate+"' AND  '"+idtpToDate+"' )";
+            string isqlOutlay = "SELECT tb_outlaydetail.*,tb_outlay.outlay_Detail,tb_employee.Em_Name FROM tb_outlaydetail,tb_outlay,tb_employee where tb_outlay.outlay_ID = tb_outlaydetail.outlay_ID AND tb_outlaydetail.Em_ID = tb_employee.Em_ID AND (STR_TO_DATE(`outlayDetail_Date`,'%Y-%m-%d') between  '" + idtpDate + "' AND  '" + idtpToDate + "' )";
             idtOutlay = iConnect.SelectByCommand(isqlOutlay);
+            ////////////////////แบบที่สอง/////////////////////////
+            //DataTable idtOutlayAmt;
+            //string isqlOutlayAmt = "SELECT tb_outlaydetail.outlay_ID,tb_outlay.outlay_Detail,tb_outlaydetail.outlayDetail_Amt FROM tb_outlaydetail,tb_outlay where tb_outlay.outlay_ID = tb_outlaydetail.outlay_ID  AND (STR_TO_DATE(`outlayDetail_Date`,'%Y-%m-%d') between  '" + idtpDate + "' AND  '" + idtpToDate + "' ) group by Outlay_ID";
+            //idtOutlayAmt = iConnect.SelectByCommand(isqlOutlayAmt);
             ////////////////////////////////////////////////////
             DataTable idtOutlayTotal;
             string isqlOutlayTotal = "SELECT sum(outlayDetail_Amt) as outlayDetail_Total FROM petshop.tb_outlaydetail where (STR_TO_DATE(`outlayDetail_Date`,'%Y-%m-%d') between  '" + idtpDate + "' AND  '" + idtpToDate + "' )";
             idtOutlayTotal = iConnect.SelectByCommand(isqlOutlayTotal);
-            //MessageBox.Show(idtOutlayTotal.Rows[0].Field<decimal>(0).ToString());
-            ////////////////////////////////////////////////////
-            DataTable idtCompany;
-            string isqlCompany = "SELECT * FROM petshop.tb_company";
-            idtCompany = iConnect.SelectByCommand(isqlCompany);
-            ////////////////////////////////////////////////////
+            ///////////////////ประกาศช่วงบนหัวเอกสาร///////////////////////
             DataTable idtHead = new DataTable("date");
             DataRow dr = null;
             idtHead.Columns.Add(new DataColumn("ReportDate", typeof(string)));
@@ -60,28 +64,62 @@ namespace Petshop
             dr["ReportDate"] = ReportDate;
             dr["ReportToDate"] = ReportToDate;
             idtHead.Rows.Add(dr);
-
-
-
+            /////////////////////////////////////////////////////////
             ReportDocument rpt = new ReportDocument();
-            rpt.Load("D:\\PetShop\\CrOutlay.rpt");
+            rpt.Load("CrOutlay.rpt");
             rpt.SetDataSource(idtOutlay);
             rpt.Subreports["Head_Sub_Report"].Database.Tables[0].SetDataSource(idtHead);
             rpt.Subreports["Company_Sub_Report"].Database.Tables[0].SetDataSource(idtCompany);
             rpt.Subreports["Outlay_Sub_Report"].Database.Tables[0].SetDataSource(idtOutlayTotal);
+            //rpt.Subreports["Outlay_Sub_ReportA"].Database.Tables[0].SetDataSource(idtOutlayAmt);
            
             this.crystalReportViewer1.ReportSource = rpt;
             this.crystalReportViewer1.Refresh();
-            //////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////[[รายงานส่วนที่ 2]]//////////////////////////////////////////
+          /*  DataTable idtService;
+            string isqlService = "";
+            idtService = iConnect.SelectByCommand(isqlService);
+            ///////////////////////////////////////////////////
+            DataTable idtServiceTotal;
+            string isqlServiceTotal = "";
+            idtServiceTotal = iConnect.SelectByCommand(isqlServiceTotal);
+            /////////////////////////////////////////////////////////////
+            ReportDocument rptService = new ReportDocument();
+            rptService.Load("D:\\PetShop\\CrSerMe.rpt");
+            rptService.SetDataSource(idtService);
+            rptService.Subreports["Head_Sub_Report"].Database.Tables[0].SetDataSource(idtHead);
+            rptService.Subreports["Company_Sub_Report"].Database.Tables[0].SetDataSource(idtCompany);
+            rptService.Subreports["SerMe_Sub_Report"].Database.Tables[0].SetDataSource(idtServiceTotal);
+
+            this.crystalReportViewer2.ReportSource = rptService;
+            this.crystalReportViewer2.Refresh();
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            DataTable idtMedi;
+            string isqlMedi = "";
+            idtMedi = iConnect.SelectByCommand(isqlMedi);
+            //////////////////////////////////////////////
+            DataTable idtMediTotal;
+            string isqlMediTotal = "";
+            idtMediTotal = iConnect.SelectByCommand(isqlMediTotal);
+            ///////////////////////////////////////////////////////
+            ReportDocument rptMedi = new ReportDocument();
+            rptMedi.Load("D:\\PetShop\\CrSerMe.rpt");
+            rptMedi.SetDataSource(idtMedi);
+            rptMedi.Subreports["Head_Sub_Report"].Database.Tables[0].SetDataSource(idtHead);
+            rptMedi.Subreports["Company_Sub_Report"].Database.Tables[0].SetDataSource(idtCompany);
+            rptMedi.Subreports["SerMe_Sub_Report"].Database.Tables[0].SetDataSource(idtMediTotal);
+
+            this.crystalReportViewer2.ReportSource = rptMedi;
+            this.crystalReportViewer2.Refresh(); */
+            ////////////////////////////////[[รายงานส่วนที่ 3]]///////////////////////////////////////////////
             DataTable idtProductSale;
             string isqlProductSale = "SELECT tb_productsaledetail.Product_ID,tb_Product.Product_Des,sum(productsale_Unit) "+
             "as ProductSale_Unit,tb_productsaledetail.Product_Sale,sum(tb_productsaledetail.ProductSale_Total) as ProductSale_Total " +
             "FROM tb_Product,tb_productsale,tb_productsaledetail "+
             "WHERE (tb_productsale.ProductSale_Date between '"+idtpDate+"' AND '"+idtpToDate+"') "+
             "AND (tb_Product.Product_ID = tb_productsaledetail.Product_ID) group by product_ID";
-
             idtProductSale = iConnect.SelectByCommand(isqlProductSale);
-
+            /////////////////////////////////////////////////////////////////////////////////////
             DataTable idtProductTotal;
             string isqlProductTotal = "SELECT sum(productsale_Unit) "+
             "as ProductSale_Unit,sum(tb_productsaledetail.ProductSale_Total) as SerMe_Total "+
@@ -90,7 +128,8 @@ namespace Petshop
             "AND (tb_Product.Product_ID = tb_productsaledetail.Product_ID)";
             idtProductTotal = iConnect.SelectByCommand(isqlProductTotal);
             ReportDocument rptSerMe = new ReportDocument();
-            rptSerMe.Load("D:\\PetShop\\CrSerMe.rpt");
+            ////////////////////////////////////////////////////////////////////////////////////
+            rptSerMe.Load("CrSerMe.rpt");
             rptSerMe.SetDataSource(idtProductSale);
             rptSerMe.Subreports["Head_Sub_Report"].Database.Tables[0].SetDataSource(idtHead);
             rptSerMe.Subreports["Company_Sub_Report"].Database.Tables[0].SetDataSource(idtCompany);
